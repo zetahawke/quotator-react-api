f1117731590c2ca6f584240b3f17ee2f8dda2f29
zetahawke <michel.szinavel@gmail.com>
2020-02-14 17:30:57 -0300

    Implementing fetch get raw information to contacts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Contacts from './components/contacts';
 
 class App extends Component {
   state = {
@@ -16,13 +17,7 @@ class App extends Component {
 
   render() {
     return (
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">Steve Jobs</h5>
-          <h6 class="card-subtitle mb-2 text-muted">steve@apple.com</h6>
-          <p class="card-text">Stay Hungry, Stay Foolish</p>
-        </div>
-      </div>
+      <Contacts contacts={this.state.contacts} />
     );
   }
 }
