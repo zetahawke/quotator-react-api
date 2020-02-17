50f512c94db0797bd014d5d9de436fc00d58c86c
zetahawke <michel.szinavel@gmail.com>
2020-02-17 10:17:16 -0300

    learning how to display antd component columns

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Button, Col, Row } from 'antd';
+import './App.css';
 import Contacts from './components/contacts';
 
-class App extends Component {
+class App extends React.Component {
   state = {
     contacts: []
   }
@@ -16,8 +18,34 @@ class App extends Component {
   }
 
   render() {
+    // const colCount = 4;
+    // const cols = [];
+    // for (let i = 1; i <= colCount; i++) {
+    //   cols.push(
+    //     <Col key={i.toString()} span={24 / colCount}>
+    //       <div>Column</div>
+    //     </Col>,
+    //   );
+    // }
     return (
-      <Contacts contacts={this.state.contacts} />
+      <div className="App">
+        <Row gutter={[16, 16]}>
+          <Col key={'asd'} span={24 / colCount}>
+            <div>Column</div>
+          </Col>
+          <Col key={'asd'} span={24 / colCount}>
+            <div>Column</div>
+          </Col>
+          <Col key={'asd'} span={24 / colCount}>
+            <div>Column</div>
+          </Col>
+          <Col key={'asd'} span={24 / colCount}>
+            <div>Column</div>
+          </Col>
+        </Row>
+        <Contacts contacts={this.state.contacts} />
+        <Button type="primary">Button</Button>
+      </div>
     );
   }
 }
